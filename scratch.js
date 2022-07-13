f8a099a2aa01b31311cf0e7341f7d8b808cdbc58
badvanes <82494163+badvanes@users.noreply.github.com>
2022-07-13 16:40:26 +0100

    My fourth commit

--- scratch.js
+++ scratch.js
@@ -5,7 +5,14 @@ const path = require('path');
 const exec = require('child_process').exec;
 //const sfdx = require('sfdx-node');
 
-var createScratchOrg = function(alias,targetusername, branchName, query, folderName,fileNameJson,ttl){
+//It facilitates the rapid development of Node based Web applications
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+var createScratchOrg = function(alias,targetusername, branchName,ttl){
     //Create a new local branch
     var gitBranch = `git branch ${branchName}`;
     // var gitPushBranch = `git push -u origin ${branchName}`;
@@ -14,8 +21,11 @@ var createScratchOrg = function(alias,targetusername, branchName, query, folderN
     //createFile(alias, edition, features, settings);
     var passwordScratchOrg = `sfdx force:user:password:generate --targetusername ${alias} --length 20`;
     var passwordDisplay = `sfdx force:user:display --targetusername ${alias}`;
-    var exportDataOrg = `sfdx force:data:tree:export -q "${query}" -u ${targetusername} -d ${folderName} -p`;
-    var importDataScratchOrg = `sfdx force:data:tree:import -u ${alias} -p ${folderName}${path.sep}${fileNameJson}`;
+    // var exportDataOrg = `sfdx force:data:tree:export -q "SELECT FIELDS(ALL) FROM ${Objects} LIMIT 200" -u ${targetusername} -d ${folderName} -p`;
+    // var importDataScratchOrg = `sfdx force:data:tree:import -u ${alias} -p ${folderName}${path.sep}${Objects}-plan.json`;
+    var startExportImport = `sfdx sfdmu:run --sourceusername ${targetusername} --targetusername ${alias}`;
+    var exportDataOrg = `sfdx sfdmu:run --sourceusername ${targetusername} --targetusername csvfile`;
+    var importDataScratchOrg = `sfdx sfdmu:run --sourceusername csvfile --targetusername ${targetusername}`;
 
 
 
@@ -34,6 +44,16 @@ var createScratchOrg = function(alias,targetusername, branchName, query, folderN
         sfdx.login(optionsLogin);
         // Create a new scratch org
         sfdx.create(optionsCreate);
+        // Use following console command to start the export from one Org to another:
+        exec(startExportImport, (error, stdout) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+            }
+            if (stdout) {
+                console.log(`stdout: ${stdout}`);
+            }
+           
+        });
         // export data from an org by creating a folder and files that will contain this data  
         exec(exportDataOrg, (error, stdout) => {
             if (error) {
@@ -53,17 +73,6 @@ var createScratchOrg = function(alias,targetusername, branchName, query, folderN
                 console.log(`stdout: ${stdout}`);
             }
         });
-        // //  push the new branch to the remote directory
-        // exec(gitPushBranch, (error, stdout) => {
-        //     if (error) {
-        //         console.log(`error: ${error.message}`);
-        //     }
-        //     if (stdout) {
-        //         console.log(`stdout: ${stdout}`);
-        //     }
-           
-        // });
-        // Generate a password for the new scratch org
         exec(passwordScratchOrg, (error, stdout) => {
             if (error) {
                 console.log(`error: ${error.message}`);
@@ -106,11 +115,36 @@ var createScratchOrg = function(alias,targetusername, branchName, query, folderN
 
     }
 }
-var param1 = 'alias';
-var param2 = 'badienvan@playful-otter-k30t9x.com';
-var param3 = 'scratch6';
-var param4 = 'SELECT Name,(SELECT FirstName, LastName FROM Contacts) FROM Account';
-var param5 = 'data';
-var param6 = 'Account-Contact-plan.json';
-var param7 = 1;
-createScratchOrg(param1,param2,param3,param4,param5,param6,param7);
+
+app.get('/scratch.html', function (req, res) {
+    res.sendFile( __dirname + "/" + "scratch.html" );
+ })
+ 
+ var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    
+    console.log("Example app listening at http://%s:%s", host, port)
+ })
+var responses={};
+ app.post('/process_post', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        bodyAlias:req.body.alias_name,
+        bodyOrg:req.body.org_name,
+        bodyBranch:req.body.branch_name,
+        bodyTime:req.body.time
+    };
+    responses=response;
+    console.log(response);
+
+   var paramAlias =response.bodyAlias ;
+   var paramOrg = response.bodyOrg;
+   var paramBranch = response.bodyBranch;
+   var paramTime = response.bodyTime;
+    createScratchOrg(paramAlias,paramOrg,paramBranch,paramTime);
+    res.end(JSON.stringify(response));
+ })
+
+ 
+
